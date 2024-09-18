8a40368109fc8e665c4235ae6e46631edde71ec0
TeoVex <teovega683@Gmail.com>
2024-09-17 21:29:51 -0300

    Update Codigo.java

--- Codigo.java
+++ Codigo.java
@@ -1,5 +1,9 @@
-
-public Codigo(){
-  String nombre = "TeoVex";
-
+public class Main {
+    public static void main(String[] args) {
+        // Declarar e inicializar la variable 'nombre' con el valor "TeoVex"
+        String nombre = "TeoVex";
+        
+        // Imprimir el valor de la variable 'nombre' en la consola
+        System.out.println(nombre);
+    }
 }
