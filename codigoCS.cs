17e4972235912ffd3cd507d2dc481d1831cd3e20
TeoVex <teovega683@Gmail.com>
2024-09-17 21:28:36 -0300

    CodigoCS.cs

---
+++ codigoCS.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Declarar e inicializar la variable 'nombre' con el valor "TeoVex"
+        string nombre = "TeoVex";
+        
+        // Imprimir el valor de la variable 'nombre' en la consola
+        Console.WriteLine(nombre);
+    }
+}
