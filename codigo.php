aa405a9f3ccf099610e4c2a5e65f8c464d4e9fa7
TeoVex <teovega683@Gmail.com>
2024-09-17 21:33:57 -0300

    CodigoPHP.php

---
+++ codigo.php
@@ -0,0 +1,7 @@
+<?php
+// Declarar e inicializar la variable 'nombre' con el valor "TeoVex"
+$nombre = "TeoVex";
+
+// Imprimir el valor de la variable 'nombre' en la salida estándar
+echo $nombre;
+?>
